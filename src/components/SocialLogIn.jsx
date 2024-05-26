08a1f5f5dcafc3d301a8cb822ead08a3cda7a561
Naimul Islam <naimuliu@gmail.com>
2024-05-26 20:39:49 +0600

    Google

--- src/components/SocialLogIn.jsx
+++ src/components/SocialLogIn.jsx
@@ -3,6 +3,7 @@ import { FaGoogle } from "react-icons/fa6";
 const SocialLogIn = () => {
     return (
         <div>
+            <div className="divider"></div>
             <div className="flex flex-col justify-center items-center p-8">
                 <button className="btn inline-flex items-center">
                     <FaGoogle className="text-yellow-500"></FaGoogle>
